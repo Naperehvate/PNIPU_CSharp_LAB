9fec0d19e94d08a53285e147c6323e772af37a6a
Naperehvate <tropikano647@mail.ru>
2025-03-18 11:39:35 +0500

    Task1_Lab12

---
+++ PNIPU_C#_LAB_12/Program.cs
@@ -0,0 +1,41 @@
+﻿#define RUN_TUSK_2
+
+using ClassLibrary;
+using PNIPU_C__LAB_12;
+
+
+#region Task1
+#if RUN_TUSK_1
+
+DoublyLinkedList list = new DoublyLinkedList();
+
+list.Add(new Person("Иван", 30));
+list.Add(new Worker("Петр", 25, "Инженер"));
+list.Add(new Engineer("Сергей", 40, "Техник", "IT"));
+list.Add(new Administration("Анна", 35, "HR"));
+
+// Распечатываем список
+Console.WriteLine("Список до добавления нового элемента:");
+list.PrintList();
+
+// Добавляем новый элемент после элемента с заданным именем
+list.InsertAfter("Петр", new Worker("Мария", 28, "Менеджер"));
+
+// Распечатываем список после добавления нового элемента
+Console.WriteLine("\nСписок после добавления нового элемента:");
+list.PrintList();
+
+// Удаляем список из памяти
+list.Clear();
+Console.WriteLine("\nСписок удален из памяти.");
+
+#endif
+#endregion
+
+#region Task2
+#if RUN_TUSK_2
+
+
+
+#endif
+#endregion
---
+++ PNIPU_C#_LAB_12/src/DoublyLinkedList.cs
@@ -0,0 +1,94 @@
+﻿using ClassLibrary;
+
+namespace PNIPU_C__LAB_12
+{
+    internal class Node
+    {
+        public Person Data { get; set; }
+        public Node? Next { get; set; }
+        public Node? Previous { get; set; }
+
+        public Node(Person data)
+        {
+            Data = data;
+            Next = null;
+            Previous = null;
+        }
+    }
+
+
+    internal class DoublyLinkedList
+    {
+        private Node? head;
+        private Node? tail;
+
+        public DoublyLinkedList()
+        {
+            head = null;
+            tail = null;
+        }
+
+        public void Add(Person data)
+        {
+            Node newNode = new Node(data);
+            if (head == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                tail.Next = newNode;
+                newNode.Previous = tail;
+                tail = newNode;
+            }
+        }
+
+        public void PrintList()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                current.Data.Show();
+                current = current.Next;
+            }
+        }
+
+        public void InsertAfter(string name, Person newData)
+        {
+            Node current = head;
+            while (current != null)
+            {
+                if (current.Data.Name == name)
+                {
+                    Node newNode = new Node(newData);
+                    newNode.Next = current.Next;
+                    newNode.Previous = current;
+                    if (current.Next != null)
+                    {
+                        current.Next.Previous = newNode;
+                    }
+                    current.Next = newNode;
+                    if (newNode.Next == null)
+                    {
+                        tail = newNode;
+                    }
+                    return;
+                }
+                current = current.Next;
+            }
+            Console.WriteLine($"Элемент с именем {name} не найден.");
+        }
+
+        public void Clear()
+        {
+            while (head != null)
+            {
+                Node temp = head;
+                head = head.Next;
+                temp = null;
+            }
+            tail = null;
+        }
+    }
+}
